235c9420830cfa1a796befa73ca40a668e1f5fcf
James Watkins <jameswatkins@live.ciom>
2021-12-28 18:30:25 -0500

    Update c19.py

--- c19.py
+++ c19.py
@@ -18,14 +18,13 @@ covid_status_columns = ["Reported Date","Total Cases","Percent positive tests in
 daily_cases = import_data(covid_status,"Reported Date",covid_status_columns)
 
 
-#Get Previous Day Total Cases to Calculate New Case
-daily_cases["Previous Day Total Cases"] = daily_cases["Total Cases"].shift()
 
-#Calculate New Cases
-daily_cases['Total New Cases'] = daily_cases['Total Cases'] - daily_cases['Previous Day Total Cases']
-
+#Calculate New Cases as Well as Previous Day New Cases to Get the Difference
+daily_cases['Total New Cases'] = daily_cases['Total Cases'] - daily_cases['Total Cases'].shift()
+daily_cases['Increase in Cases'] = daily_cases['Total New Cases'] - (daily_cases['Total Cases'].shift()-daily_cases['Total Cases'].shift(2))
 #Calculate 7 Day Avg
 daily_cases["Seven Day Average"] = daily_cases['Total New Cases'].rolling(7).mean()
+
 
 #Replace NaN Values With 0
 daily_cases = daily_cases.fillna(0)
@@ -33,7 +32,7 @@ daily_cases = daily_cases.fillna(0)
 #Re-Organize Columns
 
 daily_cases = daily_cases[["Reported Date","Total New Cases","Percent positive tests in last day",
-    "Seven Day Average"]]
+    "Seven Day Average","Increase in Cases"]]
 
 daily_cases.rename(columns={"Percent positive tests in last day":"Positivty Rate"}, inplace= True)
 
@@ -41,14 +40,13 @@ daily_cases.rename(columns={"Percent positive tests in last day":"Positivty Rate
 st.header("Status of COVID-19 Cases in Ontario")
 st.caption("This application shows the current status of covid-19 cases in Ontario. Data is collected from the Ontario Government website and Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis vitae nunc ultricies varius. Duis non efficitur diam. Phasellus venenatis laoreet risus. Donec commodo turpis leo, ac volutpat ante finibus ut. Sed euismod congue erat at condimentum. Nunc tincidunt tincidunt bibendum. Aliquam orci lectus, iaculis vitae erat eget, laoreet accumsan neque. Pellentesque eleifend tellus eu nisl varius posuere. Proin nibh est, auctor et convallis non, malesuada faucibus massa. Ut non ultrices mi. Proin commodo, ex ut auctor maximus, libero nisi egestas augue, eu venenatis tellus turpis a elit. Aliquam a erat fermentum, egestas risus ac, finibus velit.        ")
 
-
 #Get the Most Recent Data to Show as an Overview
 latest_data = daily_cases[(daily_cases["Reported Date"]== daily_cases["Reported Date"].max())]
 st.subheader("Current Snapshot for "+str(latest_data["Reported Date"].max()))
 
 #Add Columns and Display KPI's
 kpicol1,kpicol2,kpicol3 = st.columns(3)
-kpicol1.metric("Total New Cases",int(latest_data["Total New Cases"]))
+kpicol1.metric("Total New Cases",int(latest_data["Total New Cases"]),int(latest_data["Increase in Cases"]))
 kpicol2.metric("Positivity Rate %",latest_data["Positivty Rate"])
 kpicol3.metric("Seven Day Average (Cases Per Day)",int(latest_data["Seven Day Average"]))
 
@@ -69,6 +67,6 @@ mask = (daily_cases_trend["Reported Date"]>= start_date) & (daily_cases_trend["R
 daily_cases_trend = daily_cases_trend.loc[mask]
 daily_cases_trend.set_index("Reported Date",inplace=True)
 
-st.line_chart(daily_cases_trend)
+st.line_chart(daily_cases_trend,use_container_width=True)
 
 
