cc4bf5c12117343eec941203f2916edc40fbd054
James Watkins <jameswatkins@live.ciom>
2021-12-28 19:05:00 -0500

    Update c19.py

--- c19.py
+++ c19.py
@@ -14,17 +14,28 @@ def import_data(path,date_col,columns):
 
 #Daily COVID-19 Cases in Ontario
 covid_status = 'https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv'
-covid_status_columns = ["Reported Date","Total Cases","Percent positive tests in last day"]
+covid_status_columns = ["Reported Date","Total Cases","Percent positive tests in last day","Resolved"]
 daily_cases = import_data(covid_status,"Reported Date",covid_status_columns)
 
 
 
 #Calculate New Cases as Well as Previous Day New Cases to Get the Difference
 daily_cases['Total New Cases'] = daily_cases['Total Cases'] - daily_cases['Total Cases'].shift()
+#Get Increase/Decrease from Previous Day
 daily_cases['Increase in Cases'] = daily_cases['Total New Cases'] - (daily_cases['Total Cases'].shift()-daily_cases['Total Cases'].shift(2))
+
 #Calculate 7 Day Avg
 daily_cases["Seven Day Average"] = daily_cases['Total New Cases'].rolling(7).mean()
+#Get Increase / Decrease from Previous Day
+daily_cases["Increase in Seven Day Average"] = daily_cases["Seven Day Average"] - daily_cases["Seven Day Average"].shift()
 
+#Calculate Total Active Cases
+daily_cases["Total Active"] = daily_cases["Total Cases"] - daily_cases["Resolved"]
+#Get Increase / Decrease from Previous Day
+daily_cases["Increase in Active Cases"] = daily_cases["Total Active"] - daily_cases["Total Active"].shift()
+
+#Get Increase / Decrease in Positivity Rate from Previous Day
+daily_cases["Increase in Positivity"] = daily_cases["Percent positive tests in last day"] - daily_cases["Percent positive tests in last day"].shift()
 
 #Replace NaN Values With 0
 daily_cases = daily_cases.fillna(0)
@@ -32,23 +43,24 @@ daily_cases = daily_cases.fillna(0)
 #Re-Organize Columns
 
 daily_cases = daily_cases[["Reported Date","Total New Cases","Percent positive tests in last day",
-    "Seven Day Average","Increase in Cases"]]
+    "Seven Day Average","Increase in Cases","Increase in Seven Day Average","Total Active","Increase in Active Cases","Increase in Positivity"]]
 
-daily_cases.rename(columns={"Percent positive tests in last day":"Positivty Rate"}, inplace= True)
+daily_cases.rename(columns={"Percent positive tests in last day":"Positivity Rate"}, inplace= True)
 
 #Create heading for Streamlit App
 st.header("Status of COVID-19 Cases in Ontario")
-st.caption("This application shows the current status of covid-19 cases in Ontario. Data is collected from the Ontario Government website and Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis vitae nunc ultricies varius. Duis non efficitur diam. Phasellus venenatis laoreet risus. Donec commodo turpis leo, ac volutpat ante finibus ut. Sed euismod congue erat at condimentum. Nunc tincidunt tincidunt bibendum. Aliquam orci lectus, iaculis vitae erat eget, laoreet accumsan neque. Pellentesque eleifend tellus eu nisl varius posuere. Proin nibh est, auctor et convallis non, malesuada faucibus massa. Ut non ultrices mi. Proin commodo, ex ut auctor maximus, libero nisi egestas augue, eu venenatis tellus turpis a elit. Aliquam a erat fermentum, egestas risus ac, finibus velit.        ")
+st.caption("This application shows the current status of covid-19 cases in Ontario. Data is collected from the Ontario Government website")
 
 #Get the Most Recent Data to Show as an Overview
 latest_data = daily_cases[(daily_cases["Reported Date"]== daily_cases["Reported Date"].max())]
 st.subheader("Current Snapshot for "+str(latest_data["Reported Date"].max()))
 
-#Add Columns and Display KPI's
-kpicol1,kpicol2,kpicol3 = st.columns(3)
-kpicol1.metric("Total New Cases",int(latest_data["Total New Cases"]),int(latest_data["Increase in Cases"]))
-kpicol2.metric("Positivity Rate %",latest_data["Positivty Rate"])
-kpicol3.metric("Seven Day Average (Cases Per Day)",int(latest_data["Seven Day Average"]))
+#Add Columns and Display KPI's.
+kpicol1,kpicol2,kpicol3,kpicol4 = st.columns(4)
+kpicol1.metric("Total Active Cases",int(latest_data["Total Active"]),int(latest_data["Increase in Active Cases"]), delta_color="inverse") #increase is bad
+kpicol2.metric("Total New Cases",int(latest_data["Total New Cases"]),int(latest_data["Increase in Cases"]),delta_color="inverse")#increase is bad
+kpicol3.metric("Positivity Rate %",latest_data["Positivity Rate"],int(latest_data["Increase in Positivity"]),delta_color="inverse")#increase is bad
+kpicol4.metric("Seven Day Average",int(latest_data["Seven Day Average"]),int(latest_data["Increase in Seven Day Average"]),delta_color="inverse")#increase is bad
 
 #Create a Dataframe to Plot All-Time Daily Cases
 st.subheader("Total Cases by Day")
@@ -57,6 +69,7 @@ daily_cases_trend = daily_cases[["Reported Date","Total New Cases","Seven Day Av
 #Create a Date Picker to Filter the Daily Case Charts
 date_vals = (daily_cases_trend["Reported Date"].min(),daily_cases_trend["Reported Date"].max())
 start_date, end_date = st.date_input("Select a Time Frame",date_vals)
+
 if start_date < end_date: #Ensure Start Date is Not Greater Than End Date
     pass
 else:
@@ -67,6 +80,7 @@ mask = (daily_cases_trend["Reported Date"]>= start_date) & (daily_cases_trend["R
 daily_cases_trend = daily_cases_trend.loc[mask]
 daily_cases_trend.set_index("Reported Date",inplace=True)
 
-st.line_chart(daily_cases_trend,use_container_width=True)
+st.line_chart(daily_cases_trend)
+
 
 
