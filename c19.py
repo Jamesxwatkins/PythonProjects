76756caa383b7a955d6f0cba325266c759ff7e45
James Watkins <jameswatkins@live.ciom>
2021-12-30 18:05:55 -0500

    Update c19.py

--- c19.py
+++ c19.py
@@ -6,7 +6,6 @@ import plotly.express as px
 from streamlit.elements import text
 
 
-
 # Create a Function That Reads in a csv, Selects Relevant Columns, Formats a Date, and Then Sorts by That Date
 def import_data(path,date_col,columns):
     df = pd.read_csv(path)
@@ -42,7 +41,7 @@ daily_cases["Increase in Active Cases"] = daily_cases["Total Active"] - daily_ca
 
 #Get Increase / Decrease in Positivity Rate from Previous Day
 daily_cases["Increase in Positivity"] = daily_cases["Percent positive tests in last day"] - daily_cases["Percent positive tests in last day"].shift()
-
+daily_cases["Increase in Positivity"] = daily_cases["Increase in Positivity"].round(0)
 #Calculate New Deaths as Well as Previous Day New Deaths to Get the Difference
 daily_cases["Total New Deaths"] = daily_cases["Deaths"] - daily_cases["Deaths"].shift()
 #Get Increase/Decrease from Previous Day
@@ -51,13 +50,16 @@ daily_cases["Increase in Deaths"] = daily_cases["Total New Deaths"] - (daily_cas
 #As of Dec 30, the Science Advisory Table Suggests Only 1/8 Cases are Being Reported. Add an Approximate Case Count From That
 daily_cases["Approximate Cases"] = daily_cases["Total New Cases"] * 8
 
+#Add Column to Report Positivity Rate and Increase as String
+daily_cases["Positivity Reporting"] = daily_cases["Percent positive tests in last day"].astype(str) + "%"
+daily_cases["Increase in Positivity Reporting"] = daily_cases["Increase in Positivity"].astype(str)+"%"
 
 #Replace NaN Values With 0
 daily_cases = daily_cases.fillna(0)
 
 #Re-Organize Columns
 daily_cases = daily_cases[["Reported Date","Total New Cases","Approximate Cases","Increase in Cases","Percent positive tests in last day","Total tests completed in the last day","Increase in Positivity","Seven Day Average",
-    "Increase in Seven Day Average","Total Active","Increase in Active Cases","Number of patients hospitalized with COVID-19","Number of patients in ICU due to COVID-19","Total New Deaths","Increase in Deaths"]]
+    "Increase in Seven Day Average","Total Active","Increase in Active Cases","Number of patients hospitalized with COVID-19","Number of patients in ICU due to COVID-19","Total New Deaths","Increase in Deaths","Positivity Reporting","Increase in Positivity Reporting"]]
 
 daily_cases.rename(columns={"Percent positive tests in last day":"Positivity Rate","Number of patients hospitalized with COVID-19":"Hospitalized","Number of patients in ICU due to COVID-19":"In ICU","Total tests completed in the last day":"Total Tests"}, inplace= True)
 
@@ -76,14 +78,14 @@ st.header("Current Snapshot for "+str(latest_data["Reported Date"].max()))
 kpicol1,kpicol2,kpicol3,kpicol4 = st.columns(4)
 kpicol1.metric("Total Active Cases",int(latest_data["Total Active"]),int(latest_data["Increase in Active Cases"]), delta_color="inverse")
 kpicol2.metric("Total New Cases",int(latest_data["Total New Cases"]),int(latest_data["Increase in Cases"]),delta_color="inverse")
-kpicol3.metric("Positivity Rate %",latest_data["Positivity Rate"],int(latest_data["Increase in Positivity"]),delta_color="inverse")
+kpicol3.metric("Positivity Rate %",str(latest_data["Positivity Reporting"].iloc[-1]),str(latest_data["Increase in Positivity Reporting"].iloc[-1]),delta_color="inverse")
 kpicol4.metric("Seven Day Average",int(latest_data["Seven Day Average"]),int(latest_data["Increase in Seven Day Average"]),delta_color="inverse")
 
 #Get Latest Values for Total Cases and Approximate Cases
 todays_actual_cases = int(latest_data["Total New Cases"].iloc[-1])
 todays_approximate_cases = int(latest_data["Approximate Cases"].iloc[-1])
 
-st.caption("**NOTE:** As of December 30th 2021, the Ontario Science Table is estimating that approximately **1 in 8 cases are being reported**. Based on todays total of **{}** cases, we may actually be seeing upwards of **~{}** cases."\
+st.caption("**Note:** As of December 30th 2021, the Ontario Science Table is estimating that approximately **1 in 8 cases are being reported**. Based on todays total of **{}** cases, we may actually be seeing upwards of **~{}** cases."\
     .format(todays_actual_cases,todays_approximate_cases))
 
 #Create a Dataframe to Plot All-Time Daily Stats
@@ -128,10 +130,64 @@ daily_death_trend_chart = px.line(death_trending,
     #title="Total ICU & Deaths by Day"
     )
 
-
 #Plot the Death Trends
 trendcol2.subheader("Total ICU & Deaths by Day")
 trendcol2.plotly_chart(daily_death_trend_chart, use_container_width=True)
+
+#Import Cases by Vaccination Status 
+vaccination_case_status_data = 'https://data.ontario.ca/dataset/752ce2b7-c15a-4965-a3dc-397bf405e7cc/resource/eed63cf2-83dd-4598-b337-b288c0a89a16/download/cases_by_vac_status.csv'
+vaccination_case_status_cols = ["Date","covid19_cases_unvac","covid19_cases_partial_vac","covid19_cases_full_vac","covid19_cases_vac_unknown","cases_unvac_rate_per100K","cases_partial_vac_rate_per100K",
+    "cases_full_vac_rate_per100K","cases_unvac_rate_7ma","cases_partial_vac_rate_7ma","cases_full_vac_rate_7ma"]
+vaccination_cases = import_data(vaccination_case_status_data,"Date",vaccination_case_status_cols)
+
+#Rename Columns
+vaccination_cases.rename(columns={"Date":"Reported Date","covid19_cases_unvac":"Total Unvaccinated Cases","covid19_cases_partial_vac":"Total Partial Vaccinated Cases","covid19_cases_full_vac":"Total Full Vaccinated Cases",
+    "covid19_cases_vac_unknown":"Total Unkown Vaccine Status Cases","cases_unvac_rate_per100K":"Unvaccinated Rate Per 100k","cases_partial_vac_rate_per100K":"Partial Vaccination Rate Per 100k","cases_full_vac_rate_per100K":"Full Vaccination Rate Per 100k",
+    "cases_unvac_rate_7ma":"Unvaccinated 7 Day Avg","cases_partial_vac_rate_7ma":"Partial Vaccination 7 Day Avg","cases_full_vac_rate_7ma":"Full Vaccination 7 Day Avg"},inplace=True)
+
+#Replace NA Values With 0
+vaccination_cases = vaccination_cases.fillna(0)
+
+#Get Latest Data
+latest_vaccination_cases = vaccination_cases.copy()
+latest_vaccination_cases = latest_vaccination_cases[(latest_vaccination_cases["Reported Date"]== latest_vaccination_cases["Reported Date"].max())]
+
+#Create a Function That Makes a String Column Showing Percentage of Cases by Vaccination Status
+latest_vaccination_cases["Total Cases"] = latest_vaccination_cases["Total Unvaccinated Cases"] + latest_vaccination_cases["Total Partial Vaccinated Cases"] + latest_vaccination_cases["Total Full Vaccinated Cases"] + latest_vaccination_cases["Total Unkown Vaccine Status Cases"]
+
+def get_percentage(df,name,numerator):
+    df[name] = (df[numerator] / df["Total Cases"]) * 100
+    df[name] = df[name].round(0)
+    df[name] = df[name].astype(str) + "%"
+    return df[name]
+
+percent_unvaccinated = get_percentage(latest_vaccination_cases,"Percent Unvaccinated","Total Unvaccinated Cases")
+percent_fully_vaccinated = get_percentage(latest_vaccination_cases,"Percent Fully Vaccinated","Total Full Vaccinated Cases")
+percent_partially_vaccinated = get_percentage(latest_vaccination_cases,"Percent Partial Vaccinated","Total Partial Vaccinated Cases")
+percent_unknown = get_percentage(latest_vaccination_cases,"Percent Unknown","Total Unkown Vaccine Status Cases")
+
+#Show Latest Cases by Vaccination Status
+st.header("Overview by Vaccination Status for "+str(vaccination_cases["Reported Date"].max()))
+
+#Display Latest Cases by Vaccine Status as Metrics in a Single Frame
+full_vax,partial_vax,no_vax,unknown = st.columns(4)
+full_vax.metric("Percentage Fully Vaccinated",str(latest_vaccination_cases["Percent Fully Vaccinated"].iloc[-1]))
+partial_vax.metric("Percentage Partially Vaccinated",str(latest_vaccination_cases["Percent Partial Vaccinated"].iloc[-1]))
+no_vax.metric("Percentage Unvaccinated",str(latest_vaccination_cases["Percent Unvaccinated"].iloc[-1]))
+unknown.metric("Percentage Unknown",str(latest_vaccination_cases["Percent Unknown"].iloc[-1]))
+
+#Create a Visual of Cases Per 100k by Status
+daily_vax_cases = vaccination_cases.copy()
+daily_vax_cases = daily_vax_cases[["Reported Date","Unvaccinated Rate Per 100k","Full Vaccination Rate Per 100k","Partial Vaccination Rate Per 100k"]]
+vax_cases = px.area(daily_vax_cases, 
+    x="Reported Date", y=["Unvaccinated Rate Per 100k","Full Vaccination Rate Per 100k","Partial Vaccination Rate Per 100k"],
+    color_discrete_sequence=["#bebfbb","#00f2b5","#87dbff"], #Removed 7 day average - hex is "#9c9c9c"
+    labels={"Reported Date":"","value":"Cases Per 100k","variable":"Measure Name"},
+    #title="Total Confirmed Cases and Hospitalized by Day"
+    )
+
+st.subheader("Daily Cases Per 100k by Vaccination Status")
+st.plotly_chart(vax_cases,use_container_width=True)
 
 
 #Detailed COVID info is a massive file.
@@ -196,7 +252,7 @@ age_trend = px.line(age_trend,
 detail_chart1,detail_chart2 = st.columns(2)
 detail_chart1.subheader("% Total Cases by Age Group")
 detail_chart1.plotly_chart(current_age_breakdown,use_container_width=True)
-detail_chart2.subheader("7 Day Average of Cases by Age Group by Day")
+detail_chart2.subheader("Daily 7 Day Average of Cases by Age Group")
 detail_chart2.plotly_chart(age_trend,use_container_width=True)
 
 
@@ -246,10 +302,11 @@ detail_chart3, detail_chart4 = st.columns(2)
 detail_chart3.subheader("% Total Cases by Acquisition Type*")
 detail_chart3.plotly_chart(acquisition_overview,use_container_width=True)
 st.caption("'*' Excluding Case Counts Where Acquisition Type is Missing")
-detail_chart4.subheader("Total Cases by Acquisition Type by Day*")
+detail_chart4.subheader("Daily View of Cases by Acquisition Type*")
 detail_chart4.plotly_chart(acquisition_trend,use_container_width=True)
 
-
+#Overview on Vaccinations
+st.header("Overall Status of Vaccinations in Ontario")
 
 
 # #test expander
